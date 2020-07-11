d8e03b03013a9469b47f0dc698a7fbc867810d7c
ireneb97 <65849126+ireneb97@users.noreply.github.com>
2020-07-11 17:14:13 +0200

    Change function load world from obj file
    Now it is included a function for download progress and Id of the loaded object for animation

--- common_functions.js
+++ common_functions.js
@@ -5,72 +5,74 @@ import { GLTFLoader } from 'https://unpkg.com/three@0.118.3/examples/jsm/loaders
 
 
 // funzione da rivedere
-  export function change_world(position_portal_x,position_portal_y, position_portal_z){
-    if (camera.position.x == position_portal_x && camera.position.y == position_portal_y && camera.position.z == position_portal_z){
-      window.location.replace("../base_castle/index_castle.html");
-    }
+export function change_world(position_portal_x,position_portal_y, position_portal_z){
+  if (camera.position.x == position_portal_x && camera.position.y == position_portal_y && camera.position.z == position_portal_z){
+    window.location.replace("../base_castle/index_castle.html");
   }
+}
 
-  export function load_world(scene, camera, objects,path_obj_world, path_mtl_world, start_position_x, start_position_y, start_position_z){
-    var loader = new OBJLoader();
-    var mtlLoader = new MTLLoader();
+export function load_world(scene, camera, objects,path_obj_world, path_mtl_world, start_position_x, start_position_y, start_position_z){
+  var loader = new OBJLoader();
+  var mtlLoader = new MTLLoader();
+  camera.position.x = start_position_x;
+  camera.position.y = start_position_y;
+  camera.position.z = start_position_z;
+  new Promise((resolve) => {
+      mtlLoader.load(path_mtl_world, (materials) => {
+        resolve(materials);
+      });
+    })
+    .then((materials) => {
+      materials.preload();
+      loader.setMaterials(materials);
+      loader.load(path_obj_world, (object) => {
+        objects.push(object);
+        scene.add(object);
+      },onProgress,onError);
+    });
+    // will be useful for animations
+  return scene.children.length
+}
+
+// funzione per fare il load del mondo
+export function load_world_gltf(scene, camera, path_gltf_world, start_position_x, start_position_y, start_position_z){
+    var loader = new GLTFLoader();
     camera.position.x = start_position_x;
     camera.position.y = start_position_y;
     camera.position.z = start_position_z;
-    new Promise((resolve) => {
-        mtlLoader.load(path_mtl_world, (materials) => {
-          resolve(materials);
-        });
-      })
-      .then((materials) => {
-        materials.preload();
-        loader.setMaterials(materials);
-        loader.load(path_obj_world, (object) => {
-          objects.push(object);
-          scene.add(object);
-        });
-      });
-    }
+    loader.load( path_gltf_world, function ( gltf ) {
 
-    // funzione per fare il load del mondo
-    export function load_world_gltf(scene, camera, path_gltf_world, start_position_x, start_position_y, start_position_z){
-        var loader = new GLTFLoader();
-        camera.position.x = start_position_x;
-        camera.position.y = start_position_y;
-        camera.position.z = start_position_z;
-        loader.load( path_gltf_world, function ( gltf ) {
+    scene.add( gltf.scene );
 
-        scene.add( gltf.scene );
+    }, undefined, function ( error ) {
 
-        }, undefined, function ( error ) {
+        console.error( error );
 
-            console.error( error );
+    } );
+  }
 
-        } );
-      }
+export function load_object_gltf(scene, camera, path_gltf_object,
+                                start_position_x, start_position_y, start_position_z,
+                                start_rotation_x, start_rotation_y, start_rotation_z){
+  var loader = new GLTFLoader();
+  loader.load( path_gltf_object, function ( gltf ) {
+    var dragonModel = gltf.scene;
+    scene.add( dragonModel );
 
-      export function load_object_gltf(scene, camera, path_gltf_object,
-                                      start_position_x, start_position_y, start_position_z,
-                                      start_rotation_x, start_rotation_y, start_rotation_z){
-        var loader = new GLTFLoader();
-        loader.load( path_gltf_object, function ( gltf ) {
-          var dragonModel = gltf.scene;
-          scene.add( dragonModel );
+    dragonModel.position.x = start_position_x;
+    dragonModel.position.y = start_position_y;
+    dragonModel.position.z = start_position_z;
+    dragonModel.rotation.x = THREE.Math.degToRad(start_rotation_x);
+    dragonModel.rotation.y = THREE.Math.degToRad(start_rotation_y);
+    dragonModel.rotation.z = THREE.Math.degToRad(start_rotation_z);
 
-          dragonModel.position.x = start_position_x;
-          dragonModel.position.y = start_position_y;
-          dragonModel.position.z = start_position_z;
-          dragonModel.rotation.x = THREE.Math.degToRad(start_rotation_x);
-          dragonModel.rotation.y = THREE.Math.degToRad(start_rotation_y);
-          dragonModel.rotation.z = THREE.Math.degToRad(start_rotation_z);
+    }, undefined, function ( error ) {
 
-          }, undefined, function ( error ) {
+        console.error( error );
 
-              console.error( error );
+    } );
 
-          } );
-
-      }
+}
 
 
 
@@ -120,3 +122,11 @@ export function onKeyUp( event, movements ) {
 
   }
 };
+
+var onProgress = function (xhr) {
+  if (xhr.lengthComputable) {
+      var percentComplete = xhr.loaded / xhr.total * 100;
+      console.log(Math.round(percentComplete, 2) + '% downloaded');
+  }
+};
+var onError = function (xhr) { };
