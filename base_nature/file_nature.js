c9d7280a3e3baaa7d6278f39f60732ef621014c4
ireneb97 <65849126+ireneb97@users.noreply.github.com>
2020-07-23 16:43:12 +0200

    Add function shot_enemy

--- base_nature/file_nature.js
+++ base_nature/file_nature.js
@@ -1,13 +1,13 @@
     //Import library and loaders easiest way: link to unpkg website
     import * as THREE from 'https://unpkg.com/three@0.118.3/build/three.module.js';
     import { PointerLockControls } from 'https://unpkg.com/three@0.118.3/examples/jsm/controls/PointerLockControls.js';
-    import {load_world, onKeyUp, onKeyDown, check_collisions, load_object_gltf, weapon_movement} from '../common_functions.js';
+    import {load_world, onKeyUp, onKeyDown, check_collisions, load_object_gltf, weapon_movement, create_bullet} from '../common_functions.js';
 
     var renderer, scene, camera, controls;
     var objects = [];
     var raycaster;
 
-    var movements = [false,false,false,false,false];
+    var movements = [false,false,false,false,false,false];
 
     var prevTime = performance.now();
     var velocity = new THREE.Vector3();
@@ -22,32 +22,54 @@
     collisions['right'] = 1;
     var collisionDistance = 10;
 
+    //for bullet
+    var bulletPosition;
+    var bulletLoaded = false;
+    var toPosX, toPosY, toPosZ;
+    var worldDirection2 = new THREE.Vector3();
+    var intersect, raycasterOrigin2;
+    var raycaster2 = new THREE.Raycaster();
+    var prevShot = performance.now();
+    var nowShot = performance.now();
+    var time_shoting = 2000;
+
     var name_gun = 'gun'
+    var name_bullet = 'bullet'
+    var name_enemy = 'enemy'
+    var name_bullet_enemy = 'bullet_enemy'
+
+    var tween_torso = null;
+    var tween_arm = {dx:null,sx:null};
+    var tween_leg = {up:{dx:null,sx:null},down:{dx:null,sx:null}};
+    var loaded = false;
+    var Model;
+    var ArmDx, LegDx, LegSx, LegDx_Down, LegSx_Down;
+
+    var bulletPositionEnemy;
+    var bulletLoadedEnemy = false;
+    var toPosXEnemy, toPosYEnemy, toPosZEnemy;
+    var time_shoting_rate = 0;
+    var enemy_shooting = false;
+    var canShotEnemy = false;
 
     function controller(){
       controls = new PointerLockControls( camera, document.body );
-
 				var blocker = document.getElementById( 'blocker' );
 				var instructions = document.getElementById( 'instructions' );
 
 				instructions.addEventListener( 'click', function () {
-
-					controls.lock();
-
+          controls.lock();
+          canShotEnemy = true;
 				}, false );
 
 				controls.addEventListener( 'lock', function () {
-
 					instructions.style.display = 'none';
 					blocker.style.display = 'none';
-
 				} );
 
 				controls.addEventListener( 'unlock', function () {
-
 					blocker.style.display = 'block';
 					instructions.style.display = '';
-
 				} );
 
 				scene.add( controls.getObject() );
@@ -105,7 +127,9 @@
       //Loaders
       camera.rotation.y = start_rotation_player.y
       load_world(scene, camera, './nature.obj','./nature.mtl', start_position_player.x, start_position_player.y, start_position_player.z);
-      load_object_gltf(scene, name_gun,true,'./gun.gltf',0,4,-15,0,45,0);
+      load_object_gltf(scene, name_gun,false,'./gun.gltf',0,4,-15,0,45,0);
+      load_object_gltf(scene, name_enemy,true,'./woodsman.gltf',0,0,-15,0,90,0);
+      
       controller();
       
 
@@ -156,15 +180,165 @@
 
       }
     }
+    function shoting(){
+      camera.getWorldDirection(worldDirection2);
+      raycasterOrigin2 = new THREE.Vector3(controls.getObject().position.x, controls.getObject().position.y, controls.getObject().position.z);
+      raycaster2.set(raycasterOrigin2, worldDirection2);
+      intersect = raycaster2.intersectObjects( scene.children, true );
+      if (typeof intersect[0] !== 'undefined') {
+        if (movements[5] == true) {
+          var temp = nowShot;
+          nowShot = performance.now()
+          if((nowShot-prevShot) >= time_shoting){
+            prevShot = temp;
+            create_bullet(scene,name_bullet)
+            // To move the gun together with the camera, but translated of the right position
+            if (scene.getObjectByName(name_bullet)) {
+              var bulletModel = scene.getObjectByName(name_bullet);
+              var weaponModel = scene.getObjectByName(name_gun);
+              bulletModel.position.copy( weaponModel.position );
+              bulletModel.rotation.copy( weaponModel.rotation );
+              bulletModel.translateX( -0.2 );
+              bulletModel.translateY( 0.2 );
+              bulletModel.translateZ( -2.2 );
+            } 
+          }
+          movements[5] = false;
+        }
+        if (bulletModel && !bulletLoaded) {
+          bulletPosition = new createjs.Tween.get(bulletModel.position);
+          toPosX = intersect[0].point.x;
+          toPosY = intersect[0].point.y;
+          toPosZ = intersect[0].point.z;
+          bulletLoaded = true;
+        }
+        if (scene.getObjectByName(name_bullet) && bulletLoaded) {
+          bulletPosition.to({x:toPosX, y:toPosY, z:toPosZ}, time_shoting);
+    
+          if ((scene.getObjectByName(name_bullet).position.x == toPosX) && 
+          (scene.getObjectByName(name_bullet).position.y == toPosY) &&
+          (scene.getObjectByName(name_bullet).position.z == toPosZ)){
+            if((intersect[0].object.name !== name_bullet) &&  (scene.getObjectByName(name_enemy)) &&
+            (typeof (scene.getObjectByName(name_enemy)).getObjectByName(intersect[0].object.name) !== 'undefined')){
+              console.log('Preso');
+              enemyLifes -= 1;
+              if(enemyLifes == 0) {
+                scene.remove(scene.getObjectByName(name_enemy));
+                console.log('Morto');
+              }
+            }
+            scene.remove(scene.getObjectByName(name_bullet));
+            bulletLoaded = false;
+          }
+        }
+      }
+    }
     //Animation
     var animate = function () {
       requestAnimationFrame( animate );
       motion();
       check_collisions(controls, camera, scene, collisions, collisionDistance);
-      weapon_movement(scene,camera,name_gun,0.6, -0.3, -3);
-      console.log(scene.getObjectByName(name_gun).position)
+      weapon_movement(scene,camera,name_gun, 0.6, -0.3, -3);
+      shoting();
+      if(loaded) enemy_animation();
+      if (scene.getObjectByName(name_enemy) && !loaded) {
+        Model = scene.getObjectByName(name_enemy);
+        ArmDx = Model.getObjectByName('ArmDX');
+        LegDx = Model.getObjectByName('LegDX');
+        LegSx = Model.getObjectByName('LegSX');
+        LegDx_Down = Model.getObjectByName('LegDXDown');
+        LegSx_Down = Model.getObjectByName('LegSXDown');
+        if(Model) tween_torso = {position:new createjs.Tween.get(Model.position),rotation:new createjs.Tween.get(Model.rotation)};
+        if(ArmDx) tween_arm.dx = {position:new createjs.Tween.get(ArmDx.position),rotation:new createjs.Tween.get(ArmDx.rotation)};
+        if(LegDx) tween_leg.up.dx = {position:new createjs.Tween.get(LegDx.position),rotation:new createjs.Tween.get(LegDx.rotation)};
+        if(LegSx) tween_leg.up.sx = {position:new createjs.Tween.get(LegSx.position),rotation:new createjs.Tween.get(LegSx.rotation)};
+        if(LegSx_Down) tween_leg.down.dx = {position:new createjs.Tween.get(LegDx_Down.position),rotation:new createjs.Tween.get(LegDx_Down.rotation)};
+        if(LegDx_Down) tween_leg.down.sx = {position:new createjs.Tween.get(LegSx_Down.position),rotation:new createjs.Tween.get(LegSx_Down.rotation)};
+        if(Model && ArmDx && LegDx && LegSx && LegDx_Down && LegSx_Down)loaded = true
+      }
       renderer.render(scene, camera);
       
+    }
+    function prepare_shot(){
+      tween_arm.dx.rotation.to({x: THREE.Math.degToRad(-70), z: THREE.Math.degToRad(180)}, 1000);
+      tween_arm.dx.position.to({x: -0.9, y: 0.4, z: 0.8},1000);
+    }
+    // da rivedere
+    function walk(){
+      var distance_limit = 10;
+      var degree = 4.4;
+      if(camera.position.x - Model.initial_position.x != 0) var rot = (camera.position.z - Model.initial_position.z)/(camera.position.x - Model.initial_position.x);
+      else var rot = 0;
+      if(Model.position.x < camera.position.x + distance_limit || Model.position.z < camera.position.z - distance_limit){
+        tween_leg.up.dx.rotation.paused = false
+        tween_leg.up.sx.rotation.paused = false
+        tween_leg.up.dx.position.paused = false
+        tween_leg.up.sx.position.paused = false
+        tween_leg.up.dx.rotation.to({x: THREE.Math.degToRad(-degree)},1000).to({x: THREE.Math.degToRad(degree)},1000);
+        tween_leg.up.sx.rotation.to({x: THREE.Math.degToRad(degree)},1000).to({x: THREE.Math.degToRad(-degree)},1000);
+        tween_leg.up.dx.position.to({z: -0.2},1000).to({z: 0.2},1000);
+        tween_leg.up.sx.position.to({z: 0.2},1000).to({z: -0.2},1000);
+        Model.rotation.y = camera.rotation.y
+      }
+      else{
+        tween_leg.up.dx.rotation.paused = true
+        tween_leg.up.sx.rotation.paused = true
+        tween_leg.up.dx.position.paused = true
+        tween_leg.up.sx.position.paused = true
+        LegDx.rotation.x = 0;
+        LegSx.rotation.x = 0;
+        LegDx.position.z = 0;
+        LegSx.position.z = 0;
+      }
+      tween_torso.position.to({x:camera.position.x + distance_limit,z:camera.position.z + distance_limit},12000)
+    }
+
+    function shot_enemy(){
+      if (enemy_shooting == true) {
+          create_bullet(scene,name_bullet_enemy)
+          // To move the gun together with the camera, but translated of the right position
+          if (scene.getObjectByName(name_bullet_enemy)) {
+            var bulletModelEnemy = scene.getObjectByName(name_bullet_enemy);
+            var weaponModelEnemy = scene.getObjectByName(name_enemy);
+            bulletModelEnemy.position.copy( weaponModelEnemy.position);
+            bulletModelEnemy.rotation.copy( weaponModelEnemy.rotation);
+            bulletModelEnemy.translateX( -0.75);
+            bulletModelEnemy.translateY( 2.8);
+            bulletModelEnemy.translateZ( 2);
+          } 
+        enemy_shooting = false;
+      }
+      if (bulletModelEnemy && !bulletLoadedEnemy) {
+        bulletPositionEnemy = new createjs.Tween.get(bulletModelEnemy.position);
+        toPosXEnemy = camera.position.x;
+        toPosYEnemy = camera.position.y;
+        toPosZEnemy = camera.position.z;
+        bulletLoadedEnemy = true;
+      }
+      if (scene.getObjectByName(name_bullet_enemy) && bulletLoadedEnemy) {
+        bulletPositionEnemy.to({x:toPosXEnemy, y:toPosYEnemy, z:toPosZEnemy}, time_shoting);
+        if ((scene.getObjectByName(name_bullet_enemy).position.x == toPosXEnemy) && 
+        (scene.getObjectByName(name_bullet_enemy).position.y == toPosYEnemy) &&
+        (scene.getObjectByName(name_bullet_enemy).position.z == toPosZEnemy)){
+          if(camera.position.x == toPosXEnemy && camera.position.y == toPosYEnemy && camera.position.z == toPosZEnemy){
+            console.log('Preso')
+          }
+
+          scene.remove(scene.getObjectByName(name_bullet_enemy));
+          bulletLoadedEnemy = false;
+        }
+      }
+    }
+
+    function enemy_animation(){
+      prepare_shot()
+      //walk()
+      if(canShotEnemy) time_shoting_rate += 1;
+      if(time_shoting_rate / 100 == 1) {
+        enemy_shooting = true;
+        time_shoting_rate = 0;
+      }
+      shot_enemy()
     }
     init();
     animate();
